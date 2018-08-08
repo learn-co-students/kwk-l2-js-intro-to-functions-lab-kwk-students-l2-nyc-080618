a6d8385dab4d8e1bcaa7e757027cc6e1b97aa6c9
janineebaltazar <janine.ebaltazar@gmail.com>
2018-08-08 17:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Write your code in this file!
+function getUKTime(){
+  var result = new Date().getHours() + 5;
+  return result;
+}
 
+getUKTime();
 
+function discountTicket(originalPrice){
+  return originalPrice * .8;
+}
+
+function tweetReply(name){
+  return "Hey " + name + ", thanks so much for the support! I really appreciate it!";
+}
