e47f5221920fa6974a1f88923071301d4bd396c1
Claire Xu <claire02.xu@gmail.com>
2018-08-08 16:09:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // Write your code in this file!
+function getUKTime() {
+  var result = new Date().getHours() + 5;
+  return result;
+}
 
+getUKTime();
 
+function discountTicket(orginialPrice) {
+  var newPrice = orginialPrice*0.8;
+  return newPrice;
+}
+
+discountTicket();
+
+function tweetReply(fanName) {
+  var tweet = "Hey " + fanName + ", thanks so much for the support! I really appreciate it!"
+  return tweet;
+}
+
+tweetReply("Claire");
