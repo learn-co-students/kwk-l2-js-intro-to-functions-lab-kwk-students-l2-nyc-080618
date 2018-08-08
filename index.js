103149e7b092a2bfe4fdfc907746b2a06bdc8651
kyarato <kyarasto@gmail.com>
2018-08-08 17:38:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 // Write your code in this file!
 
+new Date().getHours() 
 
+function getUKTime(){
+  var result = new Date().getHours() + 5
+  return (result);
+}
+getUKTime();
+
+function discountTicket(originalPrice){
+  var result = originalPrice - (originalPrice*0.20);
+  return (result);
+}
+
+discountTicket();
+
+function tweetReply(userName){
+  var result = "Hey " + userName + ", thanks so much for the support! I really appreciate it!";
+  return (result);
+}
+tweetReply();
