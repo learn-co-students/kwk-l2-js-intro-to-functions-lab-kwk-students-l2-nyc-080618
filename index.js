8d3ea4da59b4b7ad3a08a0f207f807721e987b4a
Sahithi Muthyala <sahithi.muthyala@gmail.com>
2018-08-08 17:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Write your code in this file!
 
+function getUKTime(){
+  return new Date().getHours() + 5;
+}
 
+getUKTime();
+
+function discountTicket(price) {
+  return price * 0.8;
+}
+
+discountTicket();
+
+function tweetReply(fanName) {
+  return `Hey ${fanName}, thanks so much for the support! I really appreciate it!`;
+}
+
+tweetReply();
